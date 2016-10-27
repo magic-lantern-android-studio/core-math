80280e907ff7fe4a43e5f17dbadc50ef37f18962
Mark Millard <msm@wizzerworks.com>
2016-10-27 17:33:55 -0600

    Adding getter for MlRotation component of transform.

--- app/src/main/java/com/wizzer/mle/math/MlTransform.java
+++ app/src/main/java/com/wizzer/mle/math/MlTransform.java
@@ -964,7 +964,11 @@ public class MlTransform
     }
 	
 	/**
-	 * Get the rotation as an array of 4 components.
+	 * Get the rotation as an array of 3 components (theta x, theta y, theta z).
+     * <p>
+     * This algorithm can result in Gimbal lock. Use quaternion rotations to avoid
+     * this.
+     * </p>
 	 * 
 	 * @param r The result is returned in this array.
 	 */
@@ -1013,6 +1017,184 @@ public class MlTransform
                 r[i] += 360;
 	}
 
+    /**
+     * Get rotation component of transform.
+     * <p>
+     * Gets the quaternion rotation contained in the transform matrix.
+     * </p>
+     *
+     * @param rotation The output parameter containing the rotation.
+     */
+    public final void getRotation(MlRotation rotation)
+    {
+        rotation.setValue(this);
+    }
+
+    /**
+     * Get the rotation as an array of 4 components (angle, theta x, theta y, theta z).
+     * <p>
+     * This algorithm can result in Gimbal lock. Use quaternion rotations to avoid
+     * this.
+     * </p>
+     *
+     * @param r The result is returned in this array.
+     */
+    public final void getAxisAngleRotation(float r[])
+    {
+        float[][] t = new float[4][3];
+        int i, j;
+
+        // Normalize the rotation matrix portion of the transform.
+        for (i = 0; i < 3; i++)
+        {
+            float total = MlScalar.ML_SCALAR_ZERO;
+
+            for (j = 0; j < 3; j++)
+            {
+                t[i][j] = m_matrix[i][j];
+                total += t[i][j] * t[i][j];
+            }
+            if (total != MlScalar.ML_SCALAR_ZERO)
+            {
+                total = (float)Math.sqrt(total);
+                for (j = 0; j < 3; j++)
+                    t[i][j] = t[i][j] / total;
+            }
+        }
+
+        float[][] m = new float[3][3];
+        double[] axisAngle = toAxisAngle(m);
+        r[0] = (float)axisAngle[0];
+        r[1] = (float)axisAngle[1];
+        r[2] = (float)axisAngle[2];
+        r[3] = (float)axisAngle[3];
+    }
+
+    /*
+     * This requires a pure rotation matrix 'm' as input.
+     */
+    private double[] toAxisAngle(float[][] m)
+    {
+        double angle, x, y, z;    // variables for result
+        double epsilon = 0.01; // margin to allow for rounding errors
+        double epsilon2 = 0.1; // margin to distinguish between 0 and 180 degrees
+
+        // Optional check that input is pure rotation, 'isRotationMatrix' is defined at:
+        // http://www.euclideanspace.com/maths/algebra/matrix/orthogonal/rotation/
+        assert isRotationMatrix(m) : "not valid rotation matrix" ;  // for debugging
+
+        if ((Math.abs(m[0][1] - m[1][0]) < epsilon)
+                && (Math.abs(m[0][2] - m[2][0]) < epsilon)
+                && (Math.abs(m[1][2] - m[2][1]) < epsilon))
+        {
+            // Singularity found.
+
+            // First check for identity matrix which must have +1 for all terms
+            // in leading diagonal and zero in other terms.
+            if ((Math.abs(m[0][1] + m[1][0]) < epsilon2)
+                    && (Math.abs(m[0][2] + m[2][0]) < epsilon2)
+                    && (Math.abs(m[1][2] + m[2][1]) < epsilon2)
+                    && (Math.abs(m[0][0] + m[1][1] + m[2][2]-3) < epsilon2))
+            {
+                // This singularity is identity matrix so angle = 0
+                double[] axisAngle = new double[4]; // zero angle, arbitrary axis
+                axisAngle[0] = 0; axisAngle[1] = 1; axisAngle[2] = 0; axisAngle[3] = 0;
+                return axisAngle;
+            }
+
+            // Otherwise this singularity is angle = 180.
+            angle = Math.PI;
+            double xx = (m[0][0] + 1) / 2;
+            double yy = (m[1][1] + 1) / 2;
+            double zz = (m[2][2] + 1) / 2;
+            double xy = (m[0][1] + m[1][0]) / 4;
+            double xz = (m[0][2] + m[2][0]) / 4;
+            double yz = (m[1][2] + m[2][1]) / 4;
+            if ((xx > yy) && (xx > zz)) { // m[0][0] is the largest diagonal term
+                if (xx < epsilon) {
+                    x = 0;
+                    y = 0.7071;
+                    z = 0.7071;
+                } else {
+                    x = Math.sqrt(xx);
+                    y = xy / x;
+                    z = xz / x;
+                }
+            } else if (yy > zz) { // m[1][1] is the largest diagonal term
+                if (yy < epsilon) {
+                    x = 0.7071;
+                    y = 0;
+                    z = 0.7071;
+                } else {
+                    y = Math.sqrt(yy);
+                    x = xy / y;
+                    z = yz / y;
+                }
+            } else { // m[2][2] is the largest diagonal term so base result on this
+                if (zz < epsilon) {
+                    x = 0.7071;
+                    y = 0.7071;
+                    z = 0;
+                } else {
+                    z = Math.sqrt(zz);
+                    x = xz / z;
+                    y = yz / z;
+                }
+            }
+
+            double[] axisAngle = new double[4]; // return 180 deg rotation
+            axisAngle[0] = angle; axisAngle[1] = x; axisAngle[2] = y; axisAngle[3] = z;
+            return axisAngle;
+        }
+        // As we have reached here there are no singularities so we can handle normally.
+        double s = Math.sqrt((m[2][1] - m[1][2]) * (m[2][1] - m[1][2])
+                + (m[0][2] - m[2][0]) * (m[0][2] - m[2][0])
+                + (m[1][0] - m[0][1]) * (m[1][0] - m[0][1])); // used to normalise
+        if (Math.abs(s) < 0.001) s = 1;
+        // Prevent divide by zero, should not happen if matrix is orthogonal and should be
+        // caught by singularity test above, but I've left it in just in case
+        angle = Math.acos((m[0][0] + m[1][1] + m[2][2] - 1) / 2);
+        x = (m[2][1] - m[1][2]) / s;
+        y = (m[0][2] - m[2][0]) / s;
+        z = (m[1][0] - m[0][1]) / s;
+
+        double[] axisAngle = new double[4];
+        axisAngle[0] = angle; axisAngle[1] = x; axisAngle[2] = y; axisAngle[3] = z;
+        return axisAngle;
+    }
+
+    /*
+     * This checks that the input is a pure rotation matrix 'm'.
+     * The condition for this is:
+     * R' * R = I
+     * and
+     * det(R) =1
+     */
+    private boolean isRotationMatrix(float[][] m)
+    {
+        double epsilon = 0.01; // margin to allow for rounding errors
+        if (Math.abs(m[0][0]*m[0][1] + m[0][1]*m[1][1] + m[0][2]*m[1][2]) > epsilon) return false;
+        if (Math.abs(m[0][0]*m[2][0] + m[0][1]*m[2][1] + m[0][2]*m[2][2]) > epsilon) return false;
+        if (Math.abs(m[1][0]*m[2][0] + m[1][1]*m[2][1] + m[1][2]*m[2][2]) > epsilon) return false;
+        if (Math.abs(m[0][0]*m[0][0] + m[0][1]*m[0][1] + m[0][2]*m[0][2] - 1) > epsilon) return false;
+        if (Math.abs(m[1][0]*m[1][0] + m[1][1]*m[1][1] + m[1][2]*m[1][2] - 1) > epsilon) return false;
+        if (Math.abs(m[2][0]*m[2][0] + m[2][1]*m[2][1] + m[2][2]*m[2][2] - 1) > epsilon) return false;
+        return (Math.abs(det(m)-1) < epsilon);
+        // det is defined here:
+        // http://www.euclideanspace.com/maths/algebra/matrix/functions/determinant/threeD/
+    }
+
+    // Assumes matrix indices start from 0 (0, 1 and 2)
+    // The value of the determinant for a 3×3 matrix is:
+    //    det = m11 m22 m33 + m12 m23 m31 + m13 m21 m32 - m11 m23 m32 - m12 m 21 m33 - m13 m22 m31
+    public double det(float[][] m)
+    {
+        double value =
+            m[0][0]*m[1][1]*m[2][2] + m[0][1]*m[1][2]*m[2][0] +
+            m[0][2]*m[1][0]*m[2][1] - m[0][0]*m[1][2]*m[2][1] -
+            m[0][1]*m[1][0]*m[2][2] - m[0][2]*m[1][1]*m[2][0];
+        return value;
+    }
     /**
      * Set the rotation component of the transform.
      *
